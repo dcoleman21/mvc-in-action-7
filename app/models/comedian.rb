2ad3bbbdb9fe495644e3a3ff0981385084fd8f98
Dani Coleman <danicoleman00@gmail.com>
2023-09-07 15:41:47 -0600

    fix mvc infractions

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -5,10 +5,21 @@ class Comedian < ApplicationRecord
   validates_presence_of :age
 
   def self.average_age
-    "#{average(:age).round(2)} Years"
+    average(:age)
+    # "#{average(:age).round(2)} Years"
   end
 
   def average_special_runtime
     specials.average(:runtime)
+  end
+
+  def count_of_specials
+    specials.count
+  end
+
+  def longest_special
+    specials.maximum(:runtime)
+    # specials.order(runtime: :desc).limit(1)
+    # @longest_special = @comedian.specials.order(runtime: :desc).first
   end
 end
